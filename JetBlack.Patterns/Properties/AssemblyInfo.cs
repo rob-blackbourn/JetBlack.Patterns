2da53e9979367dc9202a6537f61c5061e0ddc911
rob-blackbourn <rob.blackbourn@googlemail.com>
2015-09-13 10:07:55 +0100

    Project creation

---
+++ JetBlack.Patterns/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JetBlack.Patterns")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("JetBlack")]
+[assembly: AssemblyProduct("JetBlack.Patterns")]
+[assembly: AssemblyCopyright("Copyright © Rob Blackbourn 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c0a9a93d-41e4-4580-a0cd-b417efc0b7bc")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
